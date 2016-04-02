fb812825d6886cf3d13d81c957e926f8b29c21ac
Safiyah <safiyah.sadiq@capitalone.com>
2016-04-02 10:57:34 -0400

    more code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+/* Get About Page */
+router.get('/resume', function(req, res) {
+  res.render('resume', {
+    title: 'Resume'
+  });
+});
+
+/* Get Projects Page */
+router.get('/project', function(req, res) {
+  res.render('project', {
+    title: 'Project'
+  });
+});
+
+/* Get Contact Page */
+router.get('/contact', function(req, res) {
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+router.post('/contact', function (req, res) {
+  res.render('thankYou');
+  title: req.body;
 });
 
 module.exports = router;
